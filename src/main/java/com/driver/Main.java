6707f265197628ed4b18f6f34e445de193f47e24
Kasturee Nayak <kasturee1998@gmail.com>
2024-03-22 01:36:48 +0530

    Encapsulation in Java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,14 @@
 package com.driver;
 
 public class Main {
-  
+  public static void main(String args[]){
+      RWOnly object=new RWOnly();
+
+      //System.out.println(object.string);
+     //java: string has private access in com.driver.RWOnly
+
+      object.setString("Encapsulation in Java");
+      String string=object.getString();
+      System.out.println(string);
+  }
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,17 @@
 package com.driver;
 
 public class RWOnly {
-    
+
+    private String string;
+
+    public String getString() {
+        return string;
+    }
+
+    public void setString(String string) {
+        this.string = string;
+    }
+
+
+
 }
